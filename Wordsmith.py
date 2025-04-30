b09c80c5af46edf1a965de761a8494d0e70d1956
JonthanBall <jonathan@jonathanballdesign.com>
2025-04-30 14:26:39 -0700

    Refactoring

--- Wordsmith.py
+++ Wordsmith.py
@@ -5,133 +5,137 @@ __doc__ = """
 Script to generate random strings from a provided set of glyphs.
 """
 
-import random
+# import random
 import vanilla
+# import nltk
+from enum import Enum
+import requests
+
+# class Dictionary(object):
+
+#     def __init__(self):
+#         nltk.download('words')
+
+#         self.englishWords = set(nltk.corpus.words.words())
+
+#     def isEnglish(self, word):
+#         return word.lower() in self.englishWords
 
 
 class Wordsmith(object):
 
     def __init__(self):
-        self.windowWidth = 500
-        self.windowHeight = 500
-        self.padding = 6
-        self.results_b_offset = 220
-        marginHorizontal = 18
-        marginVertical = 22
-        line_pos = 12
-        line_h = 30
-        col_1_left = marginHorizontal
-        col_2_left = 60
-        text_h = 20
-        label_w = col_2_left
-        label_h = text_h
-        labelSizeStyle = "small"
-        footnote_h = label_h
-        button_w = 150
-        button_h = text_h
-        button_txt = "Sync"
-        button_x1 = (self.windowWidth/2)-(button_w/2)
-        button_y1 = (-marginVertical-button_h)
-        results_txt_def = u"🟢  Ready to sync..."
-        self.results_w = self.windowWidth-(marginHorizontal*2)
-        self.results_h = self.windowHeight-self.results_b_offset
-        rule_height = 1
+        self.padding = 20
+        self.spacing = 10
+        self.buttonSize = Size(100, 20)
 
-        # Create UI Element: Window
-        windowSize = (self.windowWidth, self.windowHeight)
-        self.w = vanilla.FloatingWindow(
-            windowSize, 
-            "Sync Horizontal Metrics", 
-            minSize=windowSize, 
-            maxSize=windowSize
-        )
-        self.w.bind("resize", self.onWindowResized)
-        self.w.center()
-        self.w.open()
-        self.w.makeKey()
-
-        # Create UI Element: Label
-        self.w.label = vanilla.TextBox(
-            (col_1_left, (marginVertical+line_pos), label_w, label_h), 
-            "Label", 
-            sizeStyle=labelSizeStyle
+        buttons = [
+            vanilla.Button("auto", "Cancel", self.onCancel),
+            vanilla.Button("auto", "Submit", self.onSubmit)
+        ]
+        self.setPosSizes(
+            buttons, (self.padding, self.padding), self.buttonSize, self.spacing
         )
 
-        line_pos += line_h
+        count = len(buttons)
+        
+        width = self.padding * 2
+        width += self.buttonSize.width * count
+        width += (self.spacing * (count-1))
 
-        # Create UI Element: Horizontal Rule
-        self.w.rule_divider = vanilla.HorizontalLine(
-            (
-                marginHorizontal, 
-                (marginVertical+line_pos), 
-                -marginHorizontal, 
-                rule_height
-            )
-        )
+        height = self.padding * 2
+        height += self.buttonSize.height
 
-        line_pos += (line_h+rule_height)
+        self.window = vanilla.FloatingWindow((width, height), "Wordsmith")
+        self.window.bind("resize", self.onWindowResized)
+        self.window.center()
+        self.window.open()
+        self.window.makeKey()
+        
+        for button in buttons:
+            setattr(self.window, button.getTitle(), button)
+        
+        if len(buttons) > 0:
+            defaultButton = buttons[0].getTitle()
+            self.window.setDefaultButton(getattr(self.window, defaultButton))
 
-        # Create UI Element: Box
-        self.w.results_box = vanilla.Box(
-            (col_1_left, (marginVertical+line_pos), self.results_w, self.results_h)
-        )
-        self.w.results_box.text = vanilla.TextBox(
-            (
-                self.padding, 
-                self.padding, 
-                (self.results_w-(self.padding*2)), 
-                (self.results_h-(self.padding*2))
-            ), 
-            results_txt_def, 
-            selectable=True, 
-            sizeStyle=labelSizeStyle
-        )
+    def setPosSizes(self, elements, position, size, spacing):
+        offsetX = position[0]
 
-        # Create UI Element: Center TextBox
-        self.w.footnote = vanilla.TextBox(
-            (0, (button_y1-button_h-label_h), self.windowWidth, footnote_h), 
-            "Footnote", 
-            alignment="center", 
-            sizeStyle=labelSizeStyle
-        )
-
-        # Create UI Element: Button
-        self.w.button = vanilla.Button(
-            (button_x1, button_y1, button_w, button_h), 
-            button_txt, 
-            callback=self.onButtonPressed
-        )
-        self.w.setDefaultButton(self.w.button)
+        for element in elements:
+            element.setPosSize((offsetX, position[1], size.width, size.height))
+            offsetX += size.width + spacing
 
     def onWindowResized(self, sender):
-        win_pos = self.w.getPosSize()
-        # Get new window height
-        win_h = win_pos[3]
-        # Set results box & text dimensions
-        self.w.results_box.resize(self.results_w, win_h-self.results_b_offset)
-        self.w.results_box.text.resize(
-            (self.results_w-(self.padding*2)), 
-            win_h-self.results_b_offset
-        )
+        print("onWindowResized")
 
-    def onButtonPressed(self, sender):
-        print("Button Callback")
+    def onCancel(self, sender):
+        self.window.close()
 
-    def getLayerNames(self, font_obj):
-        layer_names = []
-        for glyph in font_obj.glyphs:
+    def onSubmit(self, sender):
+        print("onSubmit")
+
+    def getLayerNames(self, font):
+        layerNames = []
+        for glyph in font.glyphs:
             for layer in glyph.layers:
-                layer_names.append(layer.name)
-        layer_names = list(dict.fromkeys(layer_names))
-        # Return layer names list
-        return layer_names
+                layerNames.append(layer.name)
+        return list(dict.fromkeys(layerNames))
 
 
-class Size(object):
+class UINode(object):
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(
+        self, 
+        parent,
+        vanilla,
+        position,
+        size,
+        padding,
+        axis,
+        spacing=0
+    ):
+        self.parent = parent
+        self.vanilla = vanilla
+        self.position = position
+        self.size = size
+        self.padding = padding
+        self.axis = axis
+        self.spacing = spacing
+        self.children = None
+
+    def open(self):
+        pass
+
+    def close(self):
+        self.size.width += self.padding.left + self.padding.right
+        self.size.height += self.padding.top + self.padding.bottom
+        
+        parent = self.parent
+        spacing = (len(parent.children)-1) * parent.spacing
+        
+        if self.axis == Axis.HORIZONTAL:
+            self.size.width += spacing
+            parent.size.width += self.size.width
+            parent.size.height = max(self.size.height, parent.size.height)
+        elif self.axis == Axis.VERTICAL:
+            self.size.height += spacing
+            parent.size.width = max(self.size.width, parent.size.width)
+            parent.size.height += self.size.height
+
+
+class Padding(object):
+
+    def __init__(self, left, right, top, bottom):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+
+
+class Axis(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
 
 
 class Position(object):
@@ -141,13 +145,21 @@ class Position(object):
         self.y = y
 
 
-class ButtonData(object):
+class Size(object):
 
-    def __init__(self, title, position, size):
-        self.title = title
-        self.position = position
-        self.size = size
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.size = (width, height)
 
+x = requests.get('https://w3schools.com')
+print(x.status_code)
+
+# dictionary = Dictionary()
+# word = "butt"
+# isEnglish = dictionary.isEnglish(word)
+# result = f"'{word}' is a word: {isEnglish}"
+# print(result)
 
 # Run
 Wordsmith()
